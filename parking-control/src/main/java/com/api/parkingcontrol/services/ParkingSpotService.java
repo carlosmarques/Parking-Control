e8f479092adaf37d39aaf3806d5affba382f7d9a
Carlos Marques <carlosmarques@gmail.com>
2022-04-01 18:11:35 -0300

    implementando método DELETE da api

--- parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -42,4 +42,9 @@ public class ParkingSpotService {
     public Optional<ParkingSpotModel> findById(UUID id) {
         return iParkingSpotRepository.findById(id);
     }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        iParkingSpotRepository.delete(parkingSpotModel);
+    }
 }
